29b55c4eb38952cadece6b377f5414f5fe2dab85
Nikita Zheleznov <zheleznov.frontend@gmail.com>
2023-03-02 20:10:39 +0300

    feat: 🦾 upd. Block.cs

--- Assets/_src/Scripts/Block.cs
+++ Assets/_src/Scripts/Block.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Renderer MeshRenderer;
+    int points;
+  
+    public int Points
     {
-        
+        get => points;
+        set {
+            updateView(value);
+            points = value;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private Color color
     {
-        
+        get => MeshRenderer.material.GetColor(ShaderKey.COLOR);
+        set => MeshRenderer.material.SetColor(ShaderKey.COLOR, value);
+    }
+
+    private void Awake()
+    {
+        MeshRenderer = GetComponent<Renderer>();
+    }
+
+    private void updateView(int value)
+    {
+        bool isVisible = value > 0;
+        gameObject.SetActive(isVisible);
+
+        if (isVisible)
+        {
+        }
+    }
+
+    private class ShaderKey
+    {
+        public static string COLOR = "_Color";
     }
 }
