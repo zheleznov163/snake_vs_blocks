2ddb8a0a966588e89d3326839a847e7a91490202
Nikita Zheleznov <zheleznov.frontend@gmail.com>
2023-03-09 18:20:56 +0300

    feat(prefab): 📦 + MainScreen

---
+++ Assets/_src/Scripts/UI/MainScreen/MainScreen.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainScreen : MonoBehaviour
+{
+    public TMPro.TextMeshProUGUI record;
+    public TMPro.TextMeshProUGUI level;
+
+    public void setLevel(int value) =>
+        level.text = value.ToString();
+    public void setRecord(int value) =>
+        level.text = value.ToString();
+}
