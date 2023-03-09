1b7b1602ede70fff81594d142556114d1e54b274
Nikita Zheleznov <zheleznov.frontend@gmail.com>
2023-03-09 18:20:08 +0300

    feat(prefab): 📦 + GameScreen

---
+++ Assets/_src/Scripts/UI/GameScreen/GameScreen.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameScreen : MonoBehaviour
+{
+    public Slider Slider;
+    public TMPro.TextMeshProUGUI Score;
+    public TMPro.TextMeshProUGUI LevelCurrent;
+    public TMPro.TextMeshProUGUI LevelNext;
+
+    public void setScore(int score) =>
+        Score.text = score.ToString();
+
+    public void setCurrentLevel(int level)
+    {
+        LevelCurrent.text = level.ToString();
+        LevelNext.text = (level + 1).ToString();
+    }
+}
