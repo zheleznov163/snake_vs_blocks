92d4aa84112539c7dd34f828c81322127751b697
Nikita Zheleznov <zheleznov.frontend@gmail.com>
2023-03-09 17:59:24 +0300

    feat(script): 🦾 + Game

---
+++ Assets/_src/Scripts/Game.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Game : MonoBehaviour
+{
+    public Plane plane;
+    public Snake snake;
+    public Camera Camera;
+    public Control control;
+    public GameSettings settings;
+    public DB db;
+    public Level level;
+    public ScoreCounter score;
+
+    // UI
+
+    public enum Status
+    {
+        Play,
+        Win,
+        Lose,
+        Pause,
+    }
+    private Status status = Status.Pause;
+
+    public void Awake()
+    {
+        InitLevel();
+    }
+
+    public void Start()
+    {
+        snake.Died += Lose;
+        level.Finish.SnakeFinished += Win;
+        StartGame();
+    }
+
+    public void OnDestroy()
+    {
+        snake.Died -= Lose;
+        level.Finish.SnakeFinished -= Win;
+    }
+
+    private void Update()
+    {
+        switch (status)
+        {
+            case Status.Play:
+                snake.Run(settings.Speed);
+                snake.moveTail(settings.Speed * settings.SpeedTail);
+                snake.MoveByX(control.MouseDeltaX * settings.ControllSensetivity);
+
+                Camera.moveTo(snake.head.gameObject);
+                break;
+            case Status.Win:
+                snake.Run(settings.Speed);
+                snake.moveTail(settings.Speed * settings.SpeedTail);
+                break;
+
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        score.ScoreCurrent += snake.damage(settings.DamageCooldown);
+    }
+
+    private void InitLevel()
+    {
+        snake.addTail(db.SnakeLenght != 0 ? db.SnakeLenght : settings.DefaultSnakeLength);
+        level.initLevel(settings.startLevel, settings.stepsCount);
+    }
+
+    private void StartGame()
+    {
+        status = Status.Play;
+    }
+
+    private void Lose()
+    {
+        status = Status.Lose;
+        Debug.Log("LOSE");
+    }
+
+    private void Win()
+    {
+        status = Status.Win;
+
+        Debug.Log("WIN");
+    }
+}
